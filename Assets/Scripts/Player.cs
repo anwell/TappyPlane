9ec2c741878d9829b6c5dc99e678a4f082087fcf
Tichau <tsi.allard@gmail.com>
2015-04-12 19:46:21 +0200

    Add GameOver screen.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,14 @@ public class Player : MonoBehaviour
     // The force which is added when the player jumps
     // This can be changed in the Inspector window
     public Vector2 jumpForce = new Vector2(0, 300);
+
+    private bool gameOver = false;
     
     // Update is called once per frame
     private void Update()
     {
         // Jump
-        if (Input.GetKeyUp("space"))
+        if (!this.gameOver && Input.GetKeyUp("space"))
         {
             rigidbody2D.velocity = Vector2.zero;
             rigidbody2D.AddForce(jumpForce);
@@ -31,6 +33,23 @@ public class Player : MonoBehaviour
     }
 
     private void Die()
+    {
+        // We can die once time only.
+        if (this.gameOver)
+        {
+            return;
+        }
+
+        this.gameOver = true;
+
+        // Draw the Game over screen.
+        Application.LoadLevelAdditive("GameOver");
+
+        // Restart game in 3 seconds. 
+        this.Invoke("RestartGame", 3f);
+    }
+
+    private void RestartGame()
     {
         Application.LoadLevel(Application.loadedLevel);
     }
