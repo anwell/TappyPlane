e1a8b540978a29fd5da2a8e0110738784a139ea9
Tichau <tsi.allard@gmail.com>
2015-04-12 19:39:44 +0200

    Clean files.

--- Assets/Scripts/Generate.cs
+++ Assets/Scripts/Generate.cs
@@ -2,25 +2,25 @@
 
 public class Generate : MonoBehaviour
 {
-	public GameObject rocks;
-	int score = 0;
-	
-	// Use this for initialization
-	void Start()
-	{
-		InvokeRepeating("CreateObstacle", 1f, 1.5f);
-	}
-	
-	// Update is called once per frame
-	void OnGUI () 
-	{
-		GUI.color = Color.black;
-		GUILayout.Label(" Score: " + score.ToString());
-	}
-	
-	void CreateObstacle()
-	{
-		Instantiate(rocks);
-		score++;
-	}
+    public GameObject rocks;
+    int score = 0;
+    
+    // Use this for initialization
+    private void Start()
+    {
+        InvokeRepeating("CreateObstacle", 1f, 1.5f);
+    }
+    
+    // Update is called once per frame
+    private void OnGUI() 
+    {
+        GUI.color = Color.black;
+        GUILayout.Label(" Score: " + score.ToString());
+    }
+
+    private void CreateObstacle()
+    {
+        Instantiate(rocks);
+        score++;
+    }
 }
--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,13 +2,13 @@
 
 public class Obstacle : MonoBehaviour
 {
-	public Vector2 velocity = new Vector2(-4, 0);
-	public float range = 4;
-	
-	// Use this for initialization
-	void Start()
-	{
-		rigidbody2D.velocity = velocity;
-		transform.position = new Vector3(transform.position.x, transform.position.y - range * Random.value, transform.position.z);
-	}
+    public Vector2 velocity = new Vector2(-4, 0);
+    public float range = 4;
+    
+    // Use this for initialization
+    private void Start()
+    {
+        rigidbody2D.velocity = velocity;
+        transform.position = new Vector3(transform.position.x, transform.position.y - range * Random.value, transform.position.z);
+    }
 }
